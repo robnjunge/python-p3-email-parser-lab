5e3641f01d84ec3c78dd7f8ca67f2fcd63ad3e0d
robertnjunge <mubearobert@gmail.com>
2023-09-22 16:52:16 +0300

    completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, input_string):
+        self.input_string = input_string
+
+    def parse(self):
+        email_list = re.split(r'[,\s]+', self.input_string)
+        email_list = [email for email in email_list if '@' in email]
+        return sorted(set(email_list))
+
